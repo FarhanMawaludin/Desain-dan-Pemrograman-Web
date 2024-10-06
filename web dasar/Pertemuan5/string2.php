ca1293299ce90088d2ab95fc9a7de148e4d3b58d
FarhanMawaludin <farhanmawaludin7@gmail.com>
2024-10-06 20:54:43 +0700

    pertemuan 6

--- web dasar/Pertemuan5/string2.php
+++ web dasar/Pertemuan5/string2.php
@@ -1,12 +1,12 @@
 <?php
 echo "Baris\nbaru <br>"; 
-echo "Baris\rbaru <br>"; 
-echo "Halo\tDunia <br>"; 
+echo 'Baris\nbaru <br>'; 
 echo "Halo\rDunia <br>"; 
+echo 'Halo\rDunia <br>'; 
 
 echo "<pre>Halo\tDunia!</pre>"; 
-echo "<pre>Halo\rDunia!</pre>"; 
+echo '<pre>Halo\tDunia!</pre>'; 
 
-echo 'Katakanlah "Tidak pada narkoba!" <br>'; 
 echo "Katakanlah \"Tidak pada narkoba!\" <br>"; 
+echo 'Katakanlah \'Tidak pada narkoba!\' <br>'; 
 ?>
